3cc5b8e0db64aa7878913241a492f7ad7d2de176
jokLiu <liutkusjokubas@gmail.com>
2016-12-17 18:08:09 +0000

    start@

--- src/CreateAndPopulate.java
+++ src/CreateAndPopulate.java
@@ -5,47 +5,43 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 
 public class CreateAndPopulate {
 
-	
 	private BufferedReader reader;
-	private String name= "jxl706";
-	private String psw= "jvrhx1oucf";
+	private String name = "jxl706";
+	private String psw = "jvrhx1oucf";
+	private Connection conn;
 
-	public CreateAndPopulate(Connection conn)
-	{
-		//initializeRaeder();
-		//readNamePsw();
+	public CreateAndPopulate(Connection conn) {
+		// initializeRaeder();
+		// readNamePsw();
+		this.conn = conn;
 		registerDriver();
-		connectServer(conn);
-
+		connectServer();
+		deleteTables();
 
 	}
-	
-	
-	private void readNamePsw()
-	{
+
+	private void readNamePsw() {
 		String[] arg;
 		try {
-			arg =  reader.readLine().split(" ");
+			arg = reader.readLine().split(" ");
 			name = arg[0];
-			psw  = arg[1];
-			
+			psw = arg[1];
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("failed to read from the file");
 		}
-		
-		
+
 	}
-	
-	private void initializeRaeder()
-	{
+
+	private void initializeRaeder() {
 		try {
-			File file = new File("db-password.txt");	
+			File file = new File("db-password.txt");
 			reader = new BufferedReader(new FileReader(file));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -53,27 +49,48 @@ public class CreateAndPopulate {
 		}
 	}
 
-	private void registerDriver()
-	{
-		try{
-			System.setProperty("jdbc.drivers","org.postgresql.Driver");
+	private void registerDriver() {
+		try {
+			System.setProperty("jdbc.drivers", "org.postgresql.Driver");
 			Class.forName("org.postgresql.Driver");
-		}catch(ClassNotFoundException e ){
+		} catch (ClassNotFoundException e) {
 			System.out.println("Driver not found");
+			System.exit(1);
 		}
-		
+
 		System.out.println("PostgreSQL driver registered.");
 	}
 
-	private void connectServer(Connection conn)
-	{
-		try{
-			conn = DriverManager.getConnection("jdbc:postgresql://mod-intro-databases/cslibrary", name, psw);
-		}catch(SQLException e)
-		{
+	private void connectServer() {
+		try {
+			conn = DriverManager.getConnection("jdbc:postgresql://mod-intro-databases/jxl706", name, psw);
+		} catch (SQLException e) {
+			System.out.println("Ooops, couldn't get a connection");
+			System.out.println("Check that <username> & <password> are right");
+			System.exit(1);
+		}
+
+		System.out.println(conn != null ? "Database accessed!" : "Failed to make connection");
+		if (conn == null)
+			System.exit(1);
+	}
+
+	private void deleteTables() {
+		System.out.println("Deleting all the tables");
+
+		try {
+			PreparedStatement del = conn.prepareStatement("DROP SCHEMA ? CASCADE;");
+			del.setString(1, "Child");
+			del.executeQuery();
+			/*del.setString(1, "SantasLittleHelper");
+			del.executeQuery();
+			del.setString(1, "Gift");
+			del.executeQuery();
+			del.setString(1, "Present");
+			del.executeQuery();*/
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		System.out.println(conn!=null ? "Database accessed!" : "Failed to make connection");
 	}
 }
